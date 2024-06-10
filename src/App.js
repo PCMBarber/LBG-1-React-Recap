977312223ffd83d19072f24e45f3f35ae05ee713
Piers Barber <kochab.96@gmail.com>
2024-06-10 14:30:27 +0100

    pm2

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
   return (
     <Router>
       <nav>
-        <Link to='/add'>Add Pet</Link>
+        <Link to='/add'>KJGIYFDLUKOTDKYJHTGFC</Link>
         <Link to='/get'>View Pets</Link>
       </nav>
       <Routes>
